838452e4d02e83abf1ecfda6d63b7de39fd2429e
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2024-01-11 08:48:42 +0200

    Update ArrayOps.java

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,9 +1,5 @@
 public class ArrayOps {
 public static void main(String[] args) {
-    int [] array = {1,2,3};
-    int [] array1 = {1,2,3,4,5};
-    int [] array2 = {1,2,3,4,5};
-    System.out.println(isSorted(array));
         
     }
     
