e50bd09996c032ea42a18459d1816735979d7f17
danielzecharia <daniel.zecharia01@post.runi.ac.il>
2024-01-11 14:28:11 +0200

    Update StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -22,47 +22,42 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        String string = args [0];
+        String string = args[0];
         System.out.println(capVowelsLowRest(string));
 
     }
 
-    public static String capVowelsLowRest (String string) {
+    public static String capVowelsLowRest(String string) {
         // Write your code here:
-        char e='e';
-        char a='a';
-        char i='i';
-        char o= 'o';
-        char u= 'u';
+        char e = 'e';
+        char a = 'a';
+        char i = 'i';
+        char o = 'o';
+        char u = 'u';
         char temp = string.charAt(0);
-        String newS="";
-        int asci=0;
+        String newS = "";
+        int asci = 0;
 
 
-        for (int x=0; x<string.length();x++)
-        {
+        for (int x = 0; x < string.length(); x++) {
             temp = string.charAt(x);
-            if(temp==e||temp==a||temp==i||temp==o||temp==u)
-            {
-                asci= (int)temp;
-                asci = asci -32;
-                temp= (char) asci;
-            }
-            else
-            if((temp>='A')&&(temp<='Z')&&(temp!='O'&&temp!='A'&&temp!='I'&&temp!='E'&&temp!='U'))
-            {
-                asci= (int)temp;
-                asci = asci +32;
-                temp= (char) asci;
+            if (temp == e || temp == a || temp == i || temp == o || temp == u) {
+                asci = (int) temp;
+                asci = asci - 32;
+                temp = (char) asci;
+            } else if ((temp >= 'A') && (temp <= 'Z') && (temp != 'O' && temp != 'A' && temp != 'I' && temp != 'E' && temp != 'U')) {
+                asci = (int) temp;
+                asci = asci + 32;
+                temp = (char) asci;
             }
 
-            newS= newS +temp;
+            newS = newS + temp;
         }
 
         return newS;
     }
 
-    public static String camelCase (String string) {
+    public static String camelCase(String string) {
 //        // Write your code here:
 //        int asci = 0;
 //        boolean space = false;
@@ -90,11 +85,11 @@ public class StringOps {
 //        }
 //    }
     }
-    public static int[] allIndexOf (String string, char chr) {
+
+    public static int[] allIndexOf(String string, char chr) {
         // Write your code here:
-        return new int[1] ;
+        return new int[1];
     }
-
-
+}
 }
 
